fbcaf932b8a2e1827763b33be3bd21684dd654f4
Thomas Rives <thomas.rives29@gmail.com>
2020-08-09 00:06:21 +0200

    no navigation bar

--- Symptom/app/src/main/java/unlock/fr/symptom/MainActivity.java
+++ Symptom/app/src/main/java/unlock/fr/symptom/MainActivity.java
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         playButton = (ImageButton) findViewById(R.id.playButton);
         codeButton = (ImageButton) findViewById(R.id.codeButton);
         hintButton = (ImageButton) findViewById(R.id.hintButton);
--- Symptom/app/src/main/java/unlock/fr/symptom/SplashScreen.java
+++ Symptom/app/src/main/java/unlock/fr/symptom/SplashScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 
 public class SplashScreen extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         new Handler().postDelayed(new Runnable() {
 
