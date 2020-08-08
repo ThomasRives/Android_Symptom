b12086d957c8420cf1c7be19851d6468fa8ff61e
Thomas Rives <thomas.rives29@gmail.com>
2020-08-08 23:30:17 +0200

    stable version

--- Symptom/app/src/main/java/unlock/fr/symptom/SplashScreen.java
+++ Symptom/app/src/main/java/unlock/fr/symptom/SplashScreen.java
@@ -1,10 +1,10 @@
 package unlock.fr.symptom;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
     /* Time (in milliseconds) before loading the main activity */
